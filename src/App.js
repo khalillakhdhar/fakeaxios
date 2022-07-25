136541540a33fd8e0f501d4f0a0a3177547694e7
khalil <khalillakhdharatc@gmail.com>
2022-07-25 15:29:33 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
-const [data, setData] = useState(null);
-const [fetchData, setFetch] = useState(false);
-useEffect(() => {
-  if (fetchData) {
-  axios.get('https://jsonplaceholder.typicode.com/todos/1')
-  .then((res) => setData(res.data.title));
-  }
-  }, [fetchData]);
+function App() {
   return (
-  <>
-  <h1>{data}</h1>
-  <button onClick={() => setFetch(true)}>Fetch Data</button>
-  </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-  };
-  export default App;
+}
+
+export default App;
